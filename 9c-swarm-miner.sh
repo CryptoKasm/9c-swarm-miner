1dc979881891b95f158f8531d6bddfd57e9e03f1
Blaque Allen <cryptokasm@gmail.com>
2021-01-20 04:50:21 -0600

    Added Port Update

--- 9c-swarm-miner.sh
+++ 9c-swarm-miner.sh
@@ -135,7 +135,9 @@ EOF
 
 curl $COMPOSESNIPPETURL -s -o docker-compose.snippet
 source docker-compose.snippet
+mainPORT=31233
 for ((i = 1 ; i <= $NC_MINERS ; i++)); do
+    PORT=$((++mainPORT))
     composeSnippet
 done
 

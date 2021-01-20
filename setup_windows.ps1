743faee3c72794548be0ce737f4d79132fdee856
Blaque Allen <cryptokasm@gmail.com>
2021-01-20 15:45:41 -0600

    Added: Admin Elev Request (Thanks pepe) :D

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -10,6 +10,41 @@ Write-Host("  Setup Windows 10 Home for Docker")
 Write-Host("-----------------------------------------")
 
 #########################
+Function Check-RunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       Write-host "Script is running with Administrator privileges!"
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+ 
+#Check Script is running with Elevated Privileges
+Check-RunAsAdministrator
+ 
+#Place your script here.
+write-host "Welcome"
 
 # Check: WSL
 Write-Host("> Checking for Windows Subsystem for Linux...")

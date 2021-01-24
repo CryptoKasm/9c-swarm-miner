64cd47b11fa60375601ecc6dca2bbfa2637b0267
Blaque Allen <cryptokasm@gmail.com>
2021-01-23 23:50:28 -0600

    Autorun setup if no config

--- 9c-swarm-miner.sh
+++ 9c-swarm-miner.sh
@@ -3,7 +3,9 @@
 if [ -f "settings.conf" ]; then
     source settings.conf
 else
-    echo "No configuration file..."
+    echo ">No configuration file..."
+    echo ">Defaulting to Setup"
+    FIRSTRUN=1
 fi
 
 echo "--------------------------------------------"
@@ -14,7 +16,7 @@ echo "--------------------------------------------"
 if [ "$1" == "--update" ]; then
     ./bin/docker-compose.sh --Update
     exit 0
-elif [ "$1" == "--setup" ]; then
+elif [ "$1" == "--setup" ] || [ "$FIRSTRUN" == "1" ]; then
     # Check: Platform (Native Linux or WSL)
     checkPlatform() {
         if grep -q icrosoft /proc/version; then
@@ -35,12 +37,14 @@ elif [ "$1" == "--setup" ]; then
     # Check: docker-compose.yml
     ./bin/docker-compose.sh --MakeCompose
 
+    echo "> RUN SCRIPT AGAIN"
 elif [ "$1" == "--refresh" ]; then
     ./bin/snapshot.sh --RefreshSnapshot
 elif [ "$1" == "--help" ]; then
     echo "> Usage: 9c-swarm-miner.sh [OPTION]"
     echo "    --setup       Setup your system to use this script"
     echo "    --update      Update docker-compose.yml"
+    echo "    --refresh     Refresh snapshot (NATIVE LINUX ONLY)"
 else
     if [ $NC_REFRESH_SNAPSHOT == "1" ]; then
         ./bin/snapshot.sh --RefreshSnapshot
@@ -48,28 +52,37 @@ else
 
     echo "> Starting Docker Stack..."
     echo "  Please edit settings.conf before running the dockers"
+    echo "> Run this command for options: "
+    echo "     ./9c-swarm-miner.sh --help "
+
     if [ -z "$NC_PRIVATE_KEY" ]; then
         echo
-        echo "> You must set your PRIVATE_KEY before autorun is enabled!" 
+        echo "> You must set your PRIVATE_KEY before autorun is enabled!"
+        echo "  Quick Command:  $ nano settings.conf" 
+        echo
+        echo "> RUN SCRIPT AGAIN"
     else
         echo
         echo "----------"
         if [ -f "docker-compose.yml" ]; then
-            echo "Debug"
             docker-compose up -d 
         else
-            echo "   --Run command before autorun enabled:"
+            echo "   --Run command before docker can run:"
             echo "                   ./9c-swarm-miner.sh --setup "
         fi
         echo "----------"
+        echo
+        echo "  Windows Monitor (Full Log): Goto Docker and you can access logging for each individual container."
+        echo "  Windows Monitor (Mined Blocks Only): Search for Mined a block."
+        echo
+        echo "  Linux Monitor (Full Log): "
+        echo "     docker-compose logs --tail=100 -f"
+        echo "  Linux Monitor (Mined Blocks Only): "
+        echo "     docker-compose logs --tail=100 -f | grep -A 10 --color -i 'Mined a block'"
+        echo "  Linux Monitor (Mined/Reorg/Append failed events): "
+        echo "     docker-compose logs --tail=1 -f | grep --color -i -E 'Mined a|reorged|Append failed'"
+        echo
     fi
-    echo
-    echo "  Windows Monitor (Full Log): Goto Docker and you can access logging for each individual container."
-    echo "  Windows Monitor (Mined Blocks Only): Search for Mined a block."
-    echo
-    echo "  Linux Monitor (Full Log): docker-compose logs --tail=100 -f"
-    echo "  Linux Monitor (Mined Blocks Only): docker-compose logs --tail=100 -f | grep -A 10 --color -i 'Mined a block"
-    echo "  Linux Monitor (Mined/Reorg/Append failed events): docker-compose logs --tail=1 -f | grep --color -i -E 'Mined a|reorged|Append failed'"
 fi
 
 #############################################

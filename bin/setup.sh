7b66995f243738cd4c6f01682aba506bdbd1473a
Blaque Allen <cryptokasm@gmail.com>
2021-01-27 22:28:42 -0600

    Moved restart after Docker install to main script

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,6 +7,7 @@ G="\e[92m"
 Re="\e[91m"
 R="\e[0m"
 RL="\e[1A\e["
+RL2="\e[1A\e["
 
 # Exit with reason
 error_exit()
@@ -64,15 +65,11 @@ installDocker() {
             sudo usermod --append --groups docker "$USER"
             sudo systemctl enable docker
             } &> /dev/null
-            RESTART=1
         else
-            echo -e "$C  -Docker:$R$Re Make sure to run Docker Desktop on Windows 10!"$R
-            RESTART=0
-            
+            echo -e "$C  -Docker:$R$Re Make sure to run Docker Desktop on Windows 10!"$R           
         fi
     else 
-        echo -e "$RL$C  -Docker:$R$G Installed     $R"
-        RESTART=0
+        echo -e "$RL2$C  -Docker:$R$G Installed     $R"
     fi
 }
 
@@ -120,12 +117,6 @@ setupMain() {
     installDocker
     installCompose
     checkPerms
-    
-    if [[ "$RESTART" == 1 ]]; then
-        echo -e "$Re>Please log out and log in to complete the setup for Docker! Then re-run this script"$R
-        echo
-    fi
-
 }
 ###############################
 if [ "$1" == "--perms" ]; then

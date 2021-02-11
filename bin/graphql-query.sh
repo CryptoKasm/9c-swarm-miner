51f3354b3c5f348725292f679f6546e97026fad4
Blaque Allen <cryptokasm@gmail.com>
2021-02-11 12:10:43 -0600

    Add check for JQ

--- bin/graphql-query.sh
+++ bin/graphql-query.sh
@@ -4,6 +4,7 @@ source settings.conf
 
 # Queries GraphQL for NCG Balance
 checkGold() {
+    if ! [ -x "$(command -v jq)" ]; then sudo apt install jq -y &> /dev/null; fi
     graphQuery=$(curl -s -X POST -H "Content-Type: application/json" --data '{"query":"\n{\n  goldBalance(address: \"'$NC_PUBLIC_KEY'\")\n}\n"}' http://localhost:23062/graphql | jq '. | {Coins: .data.goldBalance}' | tr -d \"{}) 
     echo $graphQuery
 }

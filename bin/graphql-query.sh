47adecf0089ec8ca528d2c12387f521f1ad55809
Blaque Allen <cryptokasm@gmail.com>
2021-03-11 00:08:13 -0600

    Fixed gold query

--- bin/graphql-query.sh
+++ bin/graphql-query.sh
@@ -6,7 +6,7 @@ if ! [ -x "$(command -v jq)" ]; then sudo apt install jq -y &> /dev/null; fi
 
 # Queries GraphQL for NCG Balance
 checkGold() {
-    graphQuery=$(curl -s -X POST -H "Content-Type: application/json" --data '{"query":"\n{\n  goldBalance(address: \"'$NC_PUBLIC_KEY'\")\n}\n"}' http://localhost:23061/graphql | jq '. | {Coins: .data.goldBalance}' | tr -d \"{}) 
+    graphQuery=$(curl -s -X POST -H "Content-Type: application/json" --data '{"query":"\n{\n  goldBalance(address: \"'$NC_PUBLIC_KEY'\")\n}\n"}' http://localhost:23062/graphql | jq '. | {Coins: .data.goldBalance}' | tr -d \"{}) 
     echo $graphQuery
 }
 

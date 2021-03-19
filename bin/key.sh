8d722deaceaf6b2d5775b3cc1f532df9df4b5c65
Blaque Allen <cryptokasm@gmail.com>
2021-03-19 17:29:04 -0500

    Restyled key.sh

--- bin/key.sh
+++ bin/key.sh
@@ -10,93 +10,34 @@ PRK="$2"
 # Change Private Key
 changePrivateKey() {
     sLL
-    sTitle "Private Key update"
-    sSpacer
+    sTitle "Private Key Update"
+
     OLDKEY=$(grep 'NC_PRIVATE_KEY=' settings.conf | sed 's/^.*=//')
-    read -r -p "$(echo -e "$S""> You're current key: ""$OLDKEY"" would you like to update this? (Y/n)?: ""$RS")" PrivateUpdate
+    sAction "Current Private Key: $OLDKEY"
+    sSpacer
+
+    read -r -p "$(echo -e "$P"">$S Would you like to update this? (Y/n): ""$RS")" PrivateUpdate
     if [[ $PrivateUpdate == [yY] || $PrivateUpdate == [yY][eE][sS] ]]; then
-        read -r -p "$(echo -e "$P""|$sB SECRET_KEY: ""$RS")" NewPrK
+        read -r -p "$(echo -e "$P""|$sB   SECRET_KEY: ""$RS")" NewPrK
         sSpacer
-        read -r -p "$(echo -e "$S""> You're new key: ""$NewPrK"" is this correct?: ""$RS")" NewPrvKey
+        read -r -p "$(echo -e "$P"">$S Is this Private Key correct: $P""$NewPrK""$S? (Y/n): ""$RS")" NewPrvKey
         if [[ $NewPrvKey == [yY] || $NewPrvKey == [yY][eE][sS] ]]; then
             $(sed -i 's/^NC_PRIVATE_KEY=.*$/NC_PRIVATE_KEY='"$NewPrK"'/' settings.conf)
             $(sed -i 's/\"--private-key=.*$/\"--private-key='"$NewPrK"'\",''/' docker-compose.yml)
             sL
-            echo -e "$P""|$sB   Key has now been updated to ""$NewPrK""""$RS"
+            echo -e "$P"">$C Private Key was successfully updated!""$RS"
+            echo -e "$P""|$S   New Private Key: $P""$NewPrK""""$RS"
             sL
+            echo -e "$P"">$S Changes will take effect on next start up!""$RS"
+            exit 0
+        else
             sSpacer
-            sL
-            echo -e "$F""|$sB   Now run \"./9c-swarm-miner.sh\" for the new key to take affect""$RS"
-            sL
-            exit 0
-        else
+            sAction "Private Key was NOT saved!"
             exit 0
         fi
     else
-        exit 0
-    fi
-}
-
-# Change Public Key
-changePrublicKey() {
-    sLL
-    sTitle "Public Key update"
-    sSpacer
-    OLDKEY=$(grep 'NC_PUBLIC_KEY=' settings.conf | sed 's/^.*=//')
-    read -r -p "$(echo -e "$S""> You're current key: ""$OLDKEY"" would you like to update this? (Y/n)?: ""$RS")" PublicUpdate
-    if [[ $PublicUpdate == [yY] || $PublicUpdate == [yY][eE][sS] ]]; then
-        read -r -p "$(echo -e "$P""|$sB PUBLIC_KEY: ""$RS")" NewPbK
         sSpacer
-        read -r -p "$(echo -e "$S""> You're new key: ""$NewPbK"" is this correct?: ""$RS")" NewPblKey
-        if [[ $NewPblKey == [yY] || $NewPblKey == [yY][eE][sS] ]]; then
-            $(sed -i 's/^NC_PUBLIC_KEY=.*$/NC_PUBLIC_KEY='"$NewPbK"'/' settings.conf)
-            sL
-            echo -e "$P""|$sB   Key has now been updated to ""$NewPbK""""$RS"
-            sL
-            exit 0
-        else
-            exit 0
-        fi
-    else
-        exit 0
-    fi
-}
-
-#!/bin/bash
-source bin/cklib.sh
-
-# Check: ROOT
-checkRoot
-
-# pre-variables
-PRK="$2"
-
-# Change Private Key
-changePrivateKey() {
-    sLL
-    sTitle "Private Key update"
-    sSpacer
-    OLDKEY=$(grep 'NC_PRIVATE_KEY=' settings.conf | sed 's/^.*=//')
-    read -r -p "$(echo -e "$S""> You're current key: ""$OLDKEY"" would you like to update this? (Y/n)?: ""$RS")" PrivateUpdate
-    if [[ $PrivateUpdate == [yY] || $PrivateUpdate == [yY][eE][sS] ]]; then
-        read -r -p "$(echo -e "$P""|$sB SECRET_KEY: ""$RS")" NewPrK
-        sSpacer
-        read -r -p "$(echo -e "$S""> You're new key: ""$NewPrK"" is this correct?: ""$RS")" NewPrvKey
-        if [[ $NewPrvKey == [yY] || $NewPrvKey == [yY][eE][sS] ]]; then
-            $(sed -i 's/^NC_PRIVATE_KEY=.*$/NC_PRIVATE_KEY='"$NewPrK"'/' settings.conf)
-            $(sed -i 's/\"--private-key=.*$/\"--private-key='"$NewPrK"'\",''/' docker-compose.yml)
-            sL
-            echo -e "$P""|$sB   Key has now been updated to ""$NewPrK""""$RS"
-            sL
-            sSpacer
-            sL
-            echo -e "$F""|$sB   Now run \"./9c-swarm-miner.sh\" for the new key to take affect""$RS"
-            sL
-            exit 0
-        else
-            exit 0
-        fi
-    else
+        sAction "No changes were made!"
         exit 0
     fi
 }
@@ -104,56 +45,82 @@ changePrivateKey() {
 # Change Public Key
 changePublicKey() {
     sLL
-    sTitle "Public Key update"
-    sSpacer
+    sTitle "Public Key Update"
+
     OLDKEY=$(grep 'NC_PUBLIC_KEY=' settings.conf | sed 's/^.*=//')
-    read -r -p "$(echo -e "$S""> You're current key: ""$OLDKEY"" would you like to update this? (Y/n)?: ""$RS")" PublicUpdate
+    sAction "Current Public Key: $OLDKEY"
+    sSpacer
+
+    read -r -p "$(echo -e "$P"">$S Would you like to update this? (Y/n): ""$RS")" PublicUpdate
     if [[ $PublicUpdate == [yY] || $PublicUpdate == [yY][eE][sS] ]]; then
-        read -r -p "$(echo -e "$P""|$sB PUBLIC_KEY: ""$RS")" NewPbK
+        read -r -p "$(echo -e "$P""|$sB   PUBLIC_KEY: ""$RS")" NewPbK
         sSpacer
-        read -r -p "$(echo -e "$S""> You're new key: ""$NewPbK"" is this correct?: ""$RS")" NewPblKey
+        read -r -p "$(echo -e "$P"">$S Is this Public Key correct: $P""$NewPbK""$S? (Y/n): ""$RS")" NewPblKey
         if [[ $NewPblKey == [yY] || $NewPblKey == [yY][eE][sS] ]]; then
             $(sed -i 's/^NC_PUBLIC_KEY=.*$/NC_PUBLIC_KEY='"$NewPbK"'/' settings.conf)
             sL
-            echo -e "$P""|$sB   Key has now been updated to ""$NewPbK""""$RS"
+            echo -e "$P"">$C Public Key was successfully updated!""$RS"
+            echo -e "$P""|$S   New Public Key: $P""$NewPbK""""$RS"
+            sL
+            echo -e "$P"">$S Changes will take effect on next start up!""$RS"
             exit 0
         else
+            sSpacer
+            sAction "Public Key was NOT saved!"
             exit 0
         fi
     else
+        sSpacer
+        sAction "No changes were made!"
         exit 0
     fi
 }
 
 changeBothKeys() {
     sLL
-    sTitle "Private & Public Key update"
-    sSpacer
+    sTitle "Private & Public Key Update"
+
     OLDPRKEY=$(grep 'NC_PRIVATE_KEY=' settings.conf | sed 's/^.*=//')
     OLDPBKEY=$(grep 'NC_PUBLIC_KEY=' settings.conf | sed 's/^.*=//')
-    read -r -p "$(echo -e "$S""> You're current private key: ""$OLDPRKEY"" & current public key: ""$OLDPBKEY"" would you like to update these? (Y/n)?: ""$RS")" PublicUpdate
+    sAction "Current Private Key: $OLDPRKEY"
+    sAction "Current Public Key: $OLDPBKEY"
+    sSpacer
+
+    read -r -p "$(echo -e "$P"">$S Would you like to update these? (Y/n): ""$RS")" PublicUpdate
     if [[ $PublicUpdate == [yY] || $PublicUpdate == [yY][eE][sS] ]]; then
-        read -r -p "$(echo -e "$P""|$sB PRIVATE_KEY: ""$RS")" NewPrK
+        read -r -p "$(echo -e "$P""|$sB   PRIVATE_KEY: ""$RS")" NewPrK
+        read -r -p "$(echo -e "$P""|$sB   PUBLIC_KEY: ""$RS")" NewPbK
         sSpacer
-        read -r -p "$(echo -e "$P""|$sB PUBLIC_KEY: ""$RS")" NewPbK
+        read -r -p "$(echo -e "$P"">$S Is this Private Key correct: $P""$NewPrK""$S? (Y/n): ""$RS")" NewPrvKey
+        if ! [[ $NewPrvKey == [yY] || $NewPrvKey == [yY][eE][sS] ]]; then
+            sSpacer
+            sAction "Keys were NOT saved!"
+            exit 0
+        fi
+
         sSpacer
-        read -r -p "$(echo -e "$S""> You're new private key: ""$NewPrK"" & new public key: ""$NewPbK"" are these correct?: ""$RS")" NewPblKey
+        read -r -p "$(echo -e "$P"">$S Is this Public Key correct: $P""$NewPbK""$S? (Y/n): ""$RS")" NewPblKey
         if [[ $NewPblKey == [yY] || $NewPblKey == [yY][eE][sS] ]]; then
             $(sed -i 's/^NC_PUBLIC_KEY=.*$/NC_PUBLIC_KEY='"$NewPbK"'/' settings.conf)
             $(sed -i 's/^NC_PRIVATE_KEY=.*$/NC_PRIVATE_KEY='"$NewPrK"'/' settings.conf)
             $(sed -i 's/\"--private-key=.*$/\"--private-key='"$NewPrK"'\",''/' docker-compose.yml)
             sL
-            echo -e "$P""|$sB   Keys have now been updated to private: ""$NewPrK"" public: ""$NewPbK""""$RS"
-            sL
+            echo -e "$P"">$C Private Key was successfully updated!""$RS"
+            echo -e "$P""|$S   New Private Key: $P""$NewPrK""""$RS"
             sSpacer
+            echo -e "$P"">$C Public Key was successfully updated!""$RS"
+            echo -e "$P""|$S   New Public Key: $P""$NewPbK""""$RS"
             sL
-            echo -e "$F""|$sB   Now run \"./9c-swarm-miner.sh\" for the new key to take affect""$RS"
-            sL
+            echo -e "$P"">$S Changes will take effect on next start up!""$RS"
             exit 0
         else
+            sSpacer
+            sAction "Keys were NOT saved!"
             exit 0
         fi
     else
+        sSpacer
+        sAction "No changes were made!"
         exit 0
     fi
 }

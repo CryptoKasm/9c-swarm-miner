9bec44601a1d4f71b1ad4d4c127bf0e6cb2edd80
Blaque Allen <cryptokasm@gmail.com>
2021-04-13 11:50:16 -0500

    Fix snapshot check to match new url

--- bin/cklib.sh
+++ bin/cklib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 Debug=0
-Version="1.7.0-beta"
+Version="1.7.1-beta"
 Project="9c-swarm-miner"
 
 #+---------------------------------------------+#
@@ -225,19 +225,18 @@ function checkBuildParams() {
     CurlSnap2=`curl -s -w '%{time_connect}' -o /dev/null $SNAPSHOT1`
 
     if [[ $CurlSnap1 > $CurlSnap2 ]]; then
-        if [[ `wget -S --spider $SNAPSHOT1.zip  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
-            SNAPSHOT=`echo $SNAPSHOT1.zip`
+        if [[ `wget -S --spider $SNAPSHOT1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+            SNAPSHOT=`echo $SNAPSHOT1`
         else
-            SNAPSHOT=`echo $SNAPSHOT0.zip`
+            SNAPSHOT=`echo $SNAPSHOT0`
         fi
     else
-        if [[ `wget -S --spider $SNAPSHOT1.zip  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
-            SNAPSHOT=`echo $SNAPSHOT0.zip`
+        if [[ `wget -S --spider $SNAPSHOT1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+            SNAPSHOT=`echo $SNAPSHOT0`
         else
-            SNAPSHOT=`echo $SNAPSHOT1.zip`
+            SNAPSHOT=`echo $SNAPSHOT1`
         fi
     fi
-
 }
 
 function optionDonate() {
@@ -272,7 +271,8 @@ function displayDonate() {
 
 ###############################################
 function ckMain() {
-   checkSettings
+   #checkSettings
+   checkBuildParams
 }
 ###############################################
 #ckMain
